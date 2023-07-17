ac6615914c3f8866d082400bde99357588d2b283
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2023-07-17 22:06:50 +0530

    Update BlockchainMessager.sol

--- Blockchain_messager/BlockchainMessager.sol
+++ Blockchain_messager/BlockchainMessager.sol
@@ -1,1 +1,23 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.15;
+
+contract BlockchainMessanger {
+    uint public changeCounter; // it will count how many times the message was changed
+
+    address  public owner;
+
+    string public message;
+
+    constructor(){
+        owner = msg.sender;
+    }
+    
+    function updateTheMessage(string memory _newMessage) public { //message update functionality
+        require(msg.sender == owner);
+        message = _newMessage;
+        changeCounter++; 
+    }
+
+
+}
