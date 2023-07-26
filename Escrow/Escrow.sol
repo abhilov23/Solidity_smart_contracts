d430e9981f7f90a848e6bed02c9703181b0baee3
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2023-07-27 03:19:02 +0530

    Create Escrow.sol

---
+++ Escrow/Escrow.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+contract Escrew {
+    address payable public buyer;
+    address payable public seller;
+    uint public deposit;
+    uint public timeToExpiry;
+    uint public startTime;
+
+    //Buyer sets up the escrew and pays the deposit
+    function Escrow(address payable _seller, uint _timeToExpiry) public{
+        buyer = msg.sender;
+        seller = _seller;
+        deposit = msg.value;
+        timeToExpiry = _timeToExpiry;
+        startTime = block.timestamp;
+    }
+
+    //buyer release deposit to seller
+    function releaseToSeller() public{
+        if (msg.sender == buyer) {
+            selfdestruct(seller);
+        }
+        else{
+            revert();
+        }
+    }
+
+    //buyer can withdraw deposit if the escrow is expired
+    function withdraw() public{
+        if(!isExpired()){
+            revert();
+        }
+        if(msg.sender == buyer){
+            selfdestruct(buyer); //finish the contract and send all the funds to buyer
+        }
+        else{
+            revert();
+        }
+    }
+
+    //seller can cancel escrow and return all the functions to buyer
+    function cancel() public{
+        if (msg.sender == seller) {
+           selfdestruct(buyer); 
+        }
+        else{
+            revert();
+        }
+    }
+
+    function isExpired() view public returns(bool){
+        if (block.timestamp > startTime + timeToExpiry) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
